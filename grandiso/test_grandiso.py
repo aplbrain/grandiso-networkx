3ab7cfd6140c2f217ebe3543cb71bc08022111b0
Jordan Matelsky <j6k4m8@gmail.com>
2020-10-15 16:16:17 -0400

    [tests] Remove unittest classes

--- grandiso/test_grandiso.py
+++ grandiso/test_grandiso.py
@@ -1,6 +1,6 @@
 import time
 import copy
-import unittest
+import pytest
 
 import networkx as nx
 from networkx.algorithms.isomorphism import DiGraphMatcher, GraphMatcher
@@ -8,7 +8,7 @@ from networkx.algorithms.isomorphism import DiGraphMatcher, GraphMatcher
 from . import find_motifs
 
 
-class TestSubgraphMatching(unittest.TestCase):
+class TestSubgraphMatching:
     def test_finds_no_triangles_in_zero_tri_graph(self):
 
         motif = nx.DiGraph()
@@ -20,7 +20,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host.add_edge("A", "B")
         host.add_edge("B", "C")
 
-        self.assertEqual(len(find_motifs(motif, host)), 0)
+        assert len(find_motifs(motif, host)) == 0
 
     def test_finds_no_rect_in_zero_rect_graph(self):
 
@@ -35,7 +35,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host.add_edge("B", "C")
         host.add_edge("C", "D")
 
-        self.assertEqual(len(find_motifs(motif, host)), 0)
+        assert len(find_motifs(motif, host)) == 0
 
     def test_finds_no_triangles_in_zero_tri_graph_with_context(self):
 
@@ -50,7 +50,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host.add_edge("A", "B")
         host.add_edge("B", "C")
 
-        self.assertEqual(len(find_motifs(motif, host)), 0)
+        assert len(find_motifs(motif, host)) == 0
 
     def test_finds_no_motifs_in_small_graph(self):
 
@@ -61,7 +61,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host = nx.DiGraph()
         host.add_edge("A", "B")
 
-        self.assertEqual(len(find_motifs(motif, host)), 0)
+        assert len(find_motifs(motif, host)) == 0
 
     def test_subgraph_equals_graph_triangle(self):
 
@@ -75,7 +75,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host.add_edge("B", "C")
         host.add_edge("C", "A")
 
-        self.assertEqual(len(find_motifs(motif, host)), 3)
+        assert len(find_motifs(motif, host)) == 3
 
     def test_subgraph_equals_graph_triangle_count_only(self):
 
@@ -89,7 +89,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host.add_edge("B", "C")
         host.add_edge("C", "A")
 
-        self.assertEqual(find_motifs(motif, host, count_only=True), 3)
+        assert find_motifs(motif, host, count_only=True) == 3
 
     def test_subgraph_equals_graph_rect(self):
 
@@ -105,7 +105,7 @@ class TestSubgraphMatching(unittest.TestCase):
         host.add_edge("C", "D")
         host.add_edge("D", "A")
 
-        self.assertEqual(len(find_motifs(motif, host)), 4)
+        assert len(find_motifs(motif, host)) == 4
 
     def test_rect_count_matches_nx(self):
 
@@ -117,9 +117,8 @@ class TestSubgraphMatching(unittest.TestCase):
         motif.add_edge("C", "D")
         motif.add_edge("D", "A")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_tri_count_matches_nx(self):
@@ -131,9 +130,8 @@ class TestSubgraphMatching(unittest.TestCase):
         motif.add_edge("B", "C")
         motif.add_edge("C", "A")
 
-        self.assertEqual(
-            find_motifs(motif, host, count_only=True),
-            len([i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert find_motifs(motif, host, count_only=True) == len(
+            [i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_two_hop_count_matches_nx(self):
@@ -144,9 +142,8 @@ class TestSubgraphMatching(unittest.TestCase):
         motif.add_edge("A", "B")
         motif.add_edge("B", "C")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_high_degree_high_density_count_matches_nx(self):
@@ -159,9 +156,8 @@ class TestSubgraphMatching(unittest.TestCase):
         motif.add_edge("A", "D")
         motif.add_edge("A", "E")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_high_degree_low_density_count_matches_nx(self):
@@ -174,13 +170,12 @@ class TestSubgraphMatching(unittest.TestCase):
         motif.add_edge("A", "D")
         motif.add_edge("A", "E")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in DiGraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
 
-class TestUndirectedSubgraphMatching(unittest.TestCase):
+class TestUndirectedSubgraphMatching:
     def test_subgraph_equals_graph_triangle(self):
 
         motif = nx.Graph()
@@ -193,7 +188,7 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         host.add_edge("B", "C")
         host.add_edge("C", "A")
 
-        self.assertEqual(len(find_motifs(motif, host)), 6)
+        assert len(find_motifs(motif, host)) == 6
 
     def test_subgraph_equals_graph_rect(self):
 
@@ -209,7 +204,7 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         host.add_edge("C", "D")
         host.add_edge("D", "A")
 
-        self.assertEqual(len(find_motifs(motif, host)), 8)
+        assert len(find_motifs(motif, host)) == 8
 
     def test_rect_count_matches_nx(self):
 
@@ -221,9 +216,8 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         motif.add_edge("C", "D")
         motif.add_edge("D", "A")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_tri_count_matches_nx(self):
@@ -235,9 +229,8 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         motif.add_edge("B", "C")
         motif.add_edge("C", "A")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_two_hop_count_matches_nx(self):
@@ -248,9 +241,8 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         motif.add_edge("A", "B")
         motif.add_edge("B", "C")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_high_degree_high_density_count_matches_nx(self):
@@ -263,9 +255,8 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         motif.add_edge("A", "D")
         motif.add_edge("A", "E")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
     def test_high_degree_low_density_count_matches_nx(self):
@@ -278,8 +269,7 @@ class TestUndirectedSubgraphMatching(unittest.TestCase):
         motif.add_edge("A", "D")
         motif.add_edge("A", "E")
 
-        self.assertEqual(
-            len(find_motifs(motif, host)),
-            len([i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]),
+        assert len(find_motifs(motif, host)) == len(
+            [i for i in GraphMatcher(host, motif).subgraph_monomorphisms_iter()]
         )
 
